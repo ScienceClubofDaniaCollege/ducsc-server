9e77cf4dcd4a9231365dfefe904ef5fb103b7abd
Nurul Huda <nurulhuda859g@gmail.com>
2019-03-15 16:02:21 +0600

    added response

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => res.send('Yahoo I am working!'))
 app.get('/register', (req, res) => {
     console.log('Request recieved..');
-    res.send(` Hi ${req.query.lname} thank you for testing our form`);
+    res.send(`<h1 align="center"> Hi ${req.query.lname} thank you for testing our form</h1>`);
     }
 );
-
+app.post('/login', (req, res) => res.send('Yahoo I am working!'+ req.body.password))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
