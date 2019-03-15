37c6664079f5c4b866425bb6fdee4d70ce140a1f
Nurul Huda <nurulhuda859g@gmail.com>
2019-03-15 19:40:10 +0600

    changed method to post

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 const express = require('express')
 const app = express()
+app.use(express.json());
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('Yahoo I am working!'))
-app.get('/register', (req, res) => {
-    res.send(`<h3 align="center" style="background-color:pink;"> Hi <em>${req.query.lname}</em> thank you for testing our form</h3>. Checkout your submitted data bellow<br>
-    first name: ${req.query.fname}<br>
-    last name: ${req.query.lname}<br>
-    email: ${req.query.email}<br>
-    password: ${req.query.password}<br>
-    batch: ${req.query.batch}<br>
-    shift: ${req.query.shift}<br>
-    section: ${req.query.section}`);
+app.post('/register', (req, res) => {
+    res.send(`<h3 align="center" style="background-color:pink;"> Hi <em>${req.body.lname}</em> thank you for testing our form</h3>. Checkout your submitted data bellow<br>
+    first name: ${req.body.fname}<br>
+    last name: ${req.body.lname}<br>
+    email: ${req.body.email}<br>
+    password: ${req.body.password}<br>
+    batch: ${req.body.batch}<br>
+    shift: ${req.body.shift}<br>
+    section: ${req.body.section}`);
     }
 );
-app.get('/login', (req, res) => {
-    let loginData = {email: req.query.email, password: req.query.password};
+app.post('/login', (req, res) => {
+    let loginData = {email: req.body.email, password: req.body.password};
     res.send(`Login is not yer implemented. Checkout your submitted data bellow<br>
-    email: ${req.query.email}<br>
-    password: ${req.query.password}`);
+    email: ${req.body.email}<br>
+    password: ${req.body.password}` + loginData);
     }
 );
 
