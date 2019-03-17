f03fb6efa349f8eaaf7e7c488cc11b7113663bb9
Nurul Huda <nurulhuda859g@gmail.com>
2019-03-17 13:12:10 +0600

    test

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express()
 app.use(express.urlencoded());
 const port = process.env.PORT || 3000
 // db pass: DNYs67BakjfdBB3, db_user: dsc
-
+// connecting to db
 mongoose.connect('mongodb://dsc:DNYs67BakjfdBB3@ds261527.mlab.com:61527/dsc-member_list',{ useNewUrlParser: true }).then(()=> console.log('Conected to DB...'));
-
+// creating schema and model
 const memberSchema = new mongoose.Schema({
     fname: String,
     lname: String,
@@ -19,25 +19,60 @@ const memberSchema = new mongoose.Schema({
 
     const Member = mongoose.model('Members', memberSchema);
 
+
+
+
+// function for creating member
     const createMember = async (memberInfo) => {
         let member = new Member(memberInfo);
         const result = await member.save();
         console.log(result);
     };
+// function for getting all members
     let allUsers;
-    const getMember = async () => {
+    const getMembers = async () => {
         const result = await Member.find();
         allUsers = result;
+    };
+// function for getting member by email
+
+    let reqUser;
+    const getMemberByEmail = async (memberEmail) => {
+        const result = await Member.find({email: memberEmail});
+        reqUser = result;
+    };
+// function for getting member by password
+
+    const getMemberByPassword = async (memberPassword) => {
+        const result = await Member.find({password: memberPassword});
+        reqUser = result;
+    };
+    
+// function for getting member by login data
+
+    const getMemberByLoginData = async (memberEmail, memberPassword) => {
+        const result = await Member.find({email:memberEmail, password: memberPassword});
+        reqUser = result;
     };
     
 
 app.get('/', (req, res) => res.send('Yahoo I am working!'));
 
 app.get('/members', (req, res) => {
-   getMember();
+    getMembers();
     console.log(allUsers);
-    
     res.send(JSON.stringify(allUsers));
+});
+// app.get('/members/:email', (req, res) => {
+//     getMemberByEmail(req.params.email);
+//     console.log(reqUser);
+//     res.send(JSON.stringify(reqUser));
+// });
+
+app.get('/members/:password', (req, res) => {
+    getMemberByPassword(req.params.password);
+    console.log(reqUser);
+    res.send(JSON.stringify(reqUser));
 });
 
 app.post('/register', (req, res) => {
@@ -54,5 +89,15 @@ app.post('/login', (req, res) => {
     password: ${req.body.password}`);
     }
 );
+
+
+app.get('/member', (req, res) => {
+    getMemberByLoginData(req.query.email, req.query.password);
+    console.log(reqUser);
+    res.send(JSON.stringify(reqUser));
+});
+
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
