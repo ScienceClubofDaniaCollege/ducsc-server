c8f1f6ef3287df9d52dd976408997ab4eb6cefe5
Nurul Huda <nurulhuda859g@gmail.com>
2019-03-20 11:58:23 +0600

    t

---
+++ public/js/loader.js
@@ -0,0 +1,17 @@
+// Start of NHA's JavaScript
+// Loader
+window.addEventListener("load", function () {
+    const loader = document.querySelector(".loader");
+    const html = document.getElementById("html");
+    const body = document.getElementById("body");
+    html.removeAttribute("class");
+    body.classList.remove("noscroll");
+    loader.className += " hidden"; // class "loader hidden"
+});
+
+//   // End of NHA's JavaScript
+
+// start of zahins javascript
+
+// end of zahins javascript
+
