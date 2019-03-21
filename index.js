793692b71e7a79551d3c8de4253d2996f1fde1f5
Nurul Huda <nurulhuda859g@gmail.com>
2019-03-21 07:38:54 +0600

    wrkng

---
+++ index.js
@@ -0,0 +1,48 @@
+const mailer = require('./modules/mailer');
+const db = require('./mudules/db');
+const upload = require('./modules/db');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+app.set('view engine', 'pug')
+app.set('views', './html')
+app.use('/static', express.static('public'));
+app.use(express.urlencoded({extended:false}));
+
+
+
+
+app.get('/', (req, res) => res.redirect('https://daniascienceclub.cf'));
+
+app.post('/register', upload('photo'), (req, res) => {
+    const createMember2 = async () => {
+        req.body.photo = req.file.filename;
+        await db.createMember(req.body);
+    }
+    createMember2();
+    res.redirect('https://daniascienceclub.cf/html/login.html')
+});
+
+app.post('/login', (req, res) => {
+    const sendMemberData = async () => {
+        userData = await db.getMemberByLoginData(req.body.email, req.body.password);
+        console.log(userData);
+        if (userData.length == 0){
+        res.status(404).send("Wrong email or password");
+        } else {res.render('index', userData);};
+    }
+    sendMemberData();
+});
+
+app.get('/members', function (req, res) {
+    async function sendMembers(){
+        userData = await db.getMembers();
+        console.log(userData);
+    res.render('index', userData)
+        }
+        sendMembers()
+  })
+
+
+app.listen(port, () => console.log(`Listening on port ${port}! http://localhost:${port}/`));
+// console.log(db);
--- modules/multer.js
+++ modules/multer.js
@@ -1,15 +1,4 @@
-const mailer = require('./mailer');
-const db = require('./modules');
-const express = require('express');
-const app = express();
-app.use(express.urlencoded({extended:false}));
-const port = process.env.PORT || 3000;
-app.set('view engine', 'pug')
-app.set('views', './html')
-var multer  = require('multer')
-app.use('/static', express.static('public'));
-
-
+const multer  = require('multer')
 
 const multerConfig = {
     
@@ -58,39 +47,6 @@ const multerConfig = {
             }
         }
       };
+const upload =  multer(multerConfig).single;
 
-
-app.get('/', (req, res) => res.redirect('https://daniascienceclub.cf'));
-
-app.post('/register', multer(multerConfig).single('photo'), (req, res) => {
-    const createMember2 = async () => {
-        req.body.photo = req.file.filename;
-        await db.createMember(req.body);
-    }
-    createMember2();
-    res.redirect('https://daniascienceclub.cf/html/login.html')
-});
-
-app.post('/login', (req, res) => {
-    const sendMemberData = async () => {
-        userData = await db.getMemberByLoginData(req.body.email, req.body.password);
-        console.log(userData);
-        if (userData.length == 0){
-        res.status(404).send("Wrong email or password");
-        } else {res.render('index', userData);};
-    }
-    sendMemberData();
-});
-
-app.get('/members', function (req, res) {
-    async function sendMembers(){
-        userData = await db.getMembers();
-        console.log(userData);
-    res.render('index', userData)
-        }
-        sendMembers()
-  })
-
-
-app.listen(port, () => console.log(`Listening on port ${port}! http://localhost:${port}/`));
-// console.log(db);
+exports = upload;
