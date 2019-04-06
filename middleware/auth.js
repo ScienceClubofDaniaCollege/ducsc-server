43db9e71522917c1f1eeb56560ec86f77b2401e1
Nurul Huda <nurulhuda859g@gmail.com>
2019-04-06 13:13:36 +0600

    no need to login if already logged in

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 function auth (req, res, next) {
     const token = req.cookies.token;
     
-    if (!token) return res.status(403).send('<br><br><br><h1>Please <a href="/login">Login</a></h1>');
+    if (!token) return res.status(403).redirect('/login');
     try {
         const decoded = jwt.verify(token, 'pk');
         req.member = decoded;
