d0708b7b511f15e8dc52358bc380a5b619be88e2
Nurul Huda <nurulhuda859g@gmail.com>
2019-04-12 20:29:49 +0600

    added db for feedback

---
+++ models/feedback.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+// creating schema and model
+const memberSchema = new mongoose.Schema({
+    name: {type: String, required: true, lowercase: true},
+    email: {type: String, required: true, lowercase: true},
+    message: {type: String, required: true}
+});
+
+// setting model
+const Feedback = mongoose.model('feedback', memberSchema);
+// Joi validation
+const validateFeedback = (feedbackInfo) => {
+    const schema = {
+            name: Joi.string().min(2).max(50).required(),
+            email: Joi.string().email().min(2).max(50).required(),
+            message: Joi.string().max(255).required()
+    }
+    return Joi.validate(feedbackInfo, schema);
+}
+
+exports.validate = validateFeedback;
+exports.Feedback  = Feedback ;
--- routes/feedback.js
+++ routes/feedback.js
@@ -1,24 +1,21 @@
 // all the neccessery packages
-const { auth } = require('../middleware/auth');
+const {Feedback} = require('../models/feedback')
 const _ = require('lodash');
-const bcrypt = require('bcrypt');
-const { thankYou } = require('../modules/tempHTML');
-const { validate } = require('../models/member');
-const config = require('config');
-const imgur = require('../modules/imgur');
-const upload = require('../modules/multer');
-const db = require('../modules/db');
-const ftp = require('../modules/ftp');
-const mailer = require('../modules/mailer');
+const { validate } = require('../models/feedback');
 const express = require('express');
 const router = express.Router();
+
 
 router.use(express.urlencoded({extended: true}));
 // login and registration endpoints
 
-router.get('/', (req, res) => {
-    
-    res.send('Feedback recieved! Here is what we got: '+JSON.stringify(req.query));
+router.get('/', async (req, res) => {
+    const { error } = validate(req.query);
+    if (error) return res.status(400).send(error.details[0].message);
+    let feedbackInfo = _.pick(req.query, ['name', 'email', 'message']);
+    const feedback = new Feedback(feedbackInfo);
+    const result = await feedback.save();
+    res.send('Feedback recieved! Here is what we saved: ' + result);
 });
 
 module.exports = router;
