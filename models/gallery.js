c6e8f36c5c9077e6ee54ee230b2c5d6979252781
nurulhuda859 <nurulhuda859g@gmail.com>
2019-06-01 11:59:16 +0600

    added gallery new

--- models/gallery.js
+++ models/gallery.js
@@ -1,28 +1,36 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const config = require("config");
+const Joi = require("joi");
 
 const schema = new mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    category: {type: String, required: true},
-    date: {type: Date, required: true},
-    src: {type: Object, required: true}
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    date: { type: Date, required: true },
+    src: { type: String, required: true },
+    thumb: { type: String, required: true }
 });
 
-const Gallery = mongoose.model('gallery', schema);
+const Gallery = mongoose.model("gallery", schema);
 
 // Joi validation
-const validateGallery = (feedbackInfo) => {
+const validateGallery = feedbackInfo => {
     const schema = {
-            title: Joi.string().min(2).max(50).required(),
-            description: Joi.string().max(255).required(),
-            category: Joi.string().max(255).required(),
-            date: Joi.date().required(),
-            src: Joi.object().required(),
-    }
+        title: Joi.string()
+            .min(2)
+            .max(50)
+            .required(),
+        description: Joi.string()
+            .max(255)
+            .required(),
+        category: Joi.string()
+            .max(255)
+            .required(),
+        date: Joi.date().required(),
+    
+    };
     return Joi.validate(feedbackInfo, schema);
-}
+};
 
 exports.Gallery = Gallery;
 exports.validate = validateGallery;
--- routes/gallery.js
+++ routes/gallery.js
@@ -12,21 +12,20 @@ const router = express.Router();
 const { Gallery, validate } = require('../models/gallery');
 
 // login and registration endpoints
-router.post('/', auth, upload.upd.single('newImage'), async (req, res) => {
+router.post('/', upload.upd.single('newImage'), async (req, res) => {
     const { error } = validate(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     try {
-        const imgurLink = await imgur.uploadImg(`public/gallery-image/${req.file.filename}`);
-        const newImages = { photo: [imgurLink, `${config.get('ftp.server-address')}/${req.file.filename}`] }
+        req.body.src = await imgur.uploadImg(`public/gallery-image/${req.file.filename}`);
+        req.body.thumb = [req.body.src.slice(0, -4), 's', req.body.src.slice(-4)].join('');
         const gallery = new Gallery(req.body);
         const result = await gallery.save();
         console.log(result);
 
-        ftp.putFile(`public/gallery-image/${req.file.filename}`, `htdocs/test/${req.file.filename}`);
-        res.send('Gallery image added');
+        res.header({ 'Access-Control-Allow-Origin': '*'}).send('Gallery image added');
     }
     catch {
-        res.send('Failed to update image...')
+        res.header({ 'Access-Control-Allow-Origin': '*'}).send('Failed to update image...')
     }
 });
 
